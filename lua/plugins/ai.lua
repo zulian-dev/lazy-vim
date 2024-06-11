1c0d8ddb1a7f0eb004a7712cbcfe6fa7655c7a15
Zulian <luiz@zulian.dev>
2024-06-11 00:20:43 -0300

    update configs

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,30 +1,35 @@
 return {
---  {
---    'codota/tabnine-nvim',
---    build = "./dl_binaries.sh",
---    config = function()
---      require('tabnine').setup({
---        disable_auto_comment = true,
---        accept_keymap = "<Tab>",
---        dismiss_keymap = "<C-]>",
---        debounce_ms = 800,
---        suggestion_color = { gui = "#808080", cterm = 244 },
---        exclude_filetypes = { "TelescopePrompt", "NvimTree" },
---        log_file_path = nil, -- absolute path to Tabnine log file
---      })
---    end
---  },
- {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim",
-      "nvim-telescope/telescope.nvim"
-    }
-  },
+  { 'github/copilot.vim' },
+  --  {
+  --    'codota/tabnine-nvim',
+  --    build = "./dl_binaries.sh",
+  --    config = function()
+  --      require('tabnine').setup({
+  --        disable_auto_comment = true,
+  --        accept_keymap = "<Tab>",
+  --        dismiss_keymap = "<C-]>",
+  --        debounce_ms = 800,
+  --        suggestion_color = { gui = "#808080", cterm = 244 },
+  --        exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+  --        log_file_path = nil, -- absolute path to Tabnine log file
+  --      })
+  --    end
+  --  },
+  -- {
+  --   "jackMort/ChatGPT.nvim",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     require("chatgpt").setup({
+  --       -- async_api_key_cmd = "pass show api/tokens/openapi"
+  --       api_key_cmd = "echo '???'"
+  --     })
+  --   end,
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "folke/trouble.nvim",
+  --     "nvim-telescope/telescope.nvim"
+  --   }
+  -- },
 }
+
