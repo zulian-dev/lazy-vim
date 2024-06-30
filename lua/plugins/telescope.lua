127b42f3f328bfe7463de59dbaa24644764a3fe7
Zulian <luiz@zulian.dev>
2024-06-29 23:51:17 -0300

    add trouble plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,14 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
+      local actions = require("telescope.actions")
+      local open_with_trouble = require("trouble.sources.telescope").open
+      local add_to_trouble = require("trouble.sources.telescope").add
+
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('i', '<c-t>', open_with_trouble, {})
+      vim.keymap.set('n', '<c-t>', open_with_trouble, {})
     end
   },
   {
