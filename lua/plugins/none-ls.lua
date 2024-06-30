bcc239afa947b19221d500120d09f260cd777b38
Zulian <luiz@zulian.dev>
2024-06-29 23:53:54 -0300

    remove unused lsps

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,16 +6,9 @@ return {
       sources = {
         -- Lua
         null_ls.builtins.formatting.stylua,
-
-        -- Spell 
-        null_ls.builtins.completion.spell,
-
+        
         -- Elixir
         null_ls.builtins.diagnostics.credo,
-
-        -- Javascript
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.prettier,
       },
     })
     vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
