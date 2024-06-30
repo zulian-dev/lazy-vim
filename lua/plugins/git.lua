2d6f8e6aaddaed3a23bef2ad60bdf094b1c2f5a0
Zulian <luiz@zulian.dev>
2024-06-29 23:54:18 -0300

    add Guitar script

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,4 +1,26 @@
+vim.api.nvim_create_user_command(
+  'Guitar',
+  function(opts)
+    local job = require('plenary.async')
+
+    print("🎸")
+    vim.cmd(':Git add %')
+    vim.cmd(':Git commit -m "' .. opts.args .. '"')
+
+    job.run(function()
+      vim.cmd(':Git push')
+      print("🎼🎵🎶🎶")
+    end
+    )
+  end,
+  { nargs = '?' }
+)
+
 return {
+  -- Git shortcuts :Git
+  { "tpope/vim-fugitive" },
+
+  -- Git signs in editor
   {
     "lewis6991/gitsigns.nvim",
     config = function()
@@ -10,7 +32,8 @@ return {
         signs = {
           add = { text = "│" },
           change = { text = "│" },
-          delete = { text = "󰍵" },
+          -- delete = { text = "󰍵" },
+          delete = { text = "╳" },
           topdelete = { text = "‾" },
           changedelete = { text = "~" },
           untracked = { text = "│" },
