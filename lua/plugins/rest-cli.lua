fcde35bf276f3755406f68723e576f827a85e172
Zulian <luiz@zulian.dev>
2024-07-09 23:33:27 -0300

    api rest cli WIP

---
+++ lua/plugins/rest-cli.lua
@@ -0,0 +1,22 @@
+-- return {
+--   {
+--     "vhyrro/luarocks.nvim",
+--     priority = 1000,
+--     config = true,
+--     opts = {
+--       rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
+--     }
+--   },
+--   {
+--     "rest-nvim/rest.nvim",
+--     ft = "http",
+--     dependencies = { "luarocks.nvim" },
+--     config = function()
+--       require("rest-nvim").setup()
+--       vim.api.nvim_set_keymap("n", "<leader>rr", "<cmd>Rest run<cr>", { desc = "Run request under the cursor", remap = true})
+--       vim.api.nvim_set_keymapmap("n", "<leader>rl", "<cmd>Rest run last<cr>", { desc = "Re-run latest request", remap = true})
+--     end,
+--   }
+-- }
+
+return {}
