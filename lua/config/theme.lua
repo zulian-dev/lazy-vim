67750ca90337c86b022f440c1bf278a3766117b7
Zulian <luiz@zulian.dev>
2024-08-09 16:48:20 -0300

    small changes

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme space-vim-dark")
-vim.g.theme_id = 45
+vim.cmd("colorscheme dracula")
+vim.g.theme_id = 25
 -- end themery block
--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -1,12 +1,12 @@
 return {
-  "zaldih/themery.nvim",
-  lazyLoad = true,
-  config = function()
-    require("themery").setup({
-      livePreview = true,
-      themes = require("plugins.themes"),
-      themeConfigFile = "~/.config/nvim/lua/config/theme.lua"
-    })
-    require('config.theme')
-  end,
+	"zaldih/themery.nvim",
+	lazyLoad = true,
+	config = function()
+		require("themery").setup({
+			livePreview = true,
+			themes = require("plugins.themes"),
+			themeConfigFile = "~/.config/nvim/lua/config/theme.lua",
+		})
+		require("config.theme")
+	end,
 }
