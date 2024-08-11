1693ca328b037f60a593522bafb1721b6aa09ebf
Zulian <luiz@zulian.dev>
2024-08-10 22:48:37 -0300

    null-ls go staticcheck

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,29 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        -- Lua
-        null_ls.builtins.formatting.stylua,
-        
-        -- Elixir
-        null_ls.builtins.diagnostics.credo,
-      },
-    })
-    vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
-  end
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- Lua
+				null_ls.builtins.formatting.stylua,
+
+				-- Elixir
+				null_ls.builtins.diagnostics.credo,
+
+				-- Go
+				-- null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.diagnostics.staticcheck,
+
+				-- Java
+				-- null_ls.builtins.diagnostics.jdtls,
+
+				-- null_ls.builtins.diagnostics.eslint.with({
+				-- 	condition = function(utils)
+				-- 		return utils.root_has_file("package.json")
+				-- 	end,
+				-- }),
+			},
+		})
+		vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+	end,
 }
