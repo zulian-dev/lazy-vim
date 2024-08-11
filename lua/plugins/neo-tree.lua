75d5504e49117fa07c8e0ca1a51943f995605bab
Zulian <luiz@zulian.dev>
2024-08-10 22:49:01 -0300

    neotree group folders to java projects

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -198,7 +198,11 @@ return {
 					--               -- the current file is changed while the tree is open.
 					leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
 				},
-				group_empty_dirs = false, -- when true, empty folders will be grouped together
+
+				-- group_empty_dirs = false, -- when true, empty folders will be grouped together
+				group_empty_dirs = true,
+				scan_mode = "deep",
+
 				hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
 				-- in whatever position is specified in window.position
 				-- "open_current",  -- netrw disabled, opening a directory opens within the
