ceba6e99b222ca9bf3aa94837c935206878f7846
Zulian <luiz@zulian.dev>
2024-08-29 02:37:15 -0300

    Plugin Cmpletions path

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -34,9 +34,9 @@ local function border(hl_name)
 end
 
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
+	{ "hrsh7th/cmp-nvim-lsp", },
+	{ "hrsh7th/cmp-path"} ,
+	{ "hrsh7th/cmp-buffer"} ,
 	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
@@ -73,8 +73,8 @@ return {
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" }, -- For luasnip users.
-				}, {
 					{ name = "buffer" },
+					{ name = 'path' },
 				}),
 
 				formatting = {
