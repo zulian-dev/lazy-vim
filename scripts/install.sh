865b6ab0f6a291af102ec5e365a19200dddcaccd
Zulian <luiz@zulian.dev>
2024-11-30 22:23:28 -0300

    update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,10 @@
 
 profile="~/.zshrc"
 
+gitFolder="~/Documents/git/"
+
+gitConfigs="https://github.com/zulian-dev/Configurations"
+
 # Função para exibir mensagens com cores
 info() {
 	echo -e "\033[1;34m$1\033[0m"
@@ -11,9 +15,20 @@ error() {
 	echo -e "\033[1;31m$1\033[0m"
 }
 
+install() {
+	tool=$1
+	info "Instalando $1..."
+	brew install $1 || {
+		error "Erro ao instalar $1."
+		exit 1
+	}
+}
+
 # Atualizando o macOS e instalando ferramentas básicas
 info "Atualizando o sistema e instalando ferramentas essenciais..."
 sudo softwareupdate --install --all
+
+###############################################################################
 
 # Instalando o Homebrew, se não estiver instalado
 if ! command -v brew &>/dev/null; then
@@ -32,39 +47,59 @@ brew update
 
 ###############################################################################
 
-# Instalando Neovim
-info "Instalando Neovim..."
-brew install neovim || {
-	error "Erro ao instalar o Neovim."
+install "curl"
+install "git"
+
+###############################################################################
+
+info "Copiando as configurações do git"
+info $gitConfigs
+
+# Cria a pasta para baixar as configurações
+mkdir -p $gitFolder
+
+# Baixa as configurações
+git clone $gitConfigs
+
+info "Configuração copiadas!"
+
+
+###############################################################################
+
+install "neovim"
+
+# Cria link simbolicos
+ln -s ~/Documents/git/Configurations/nvim ~/.config/nvim
+
+###############################################################################
+
+# Instalando o terminal Kitty
+info "Instalando o terminal Kitty..."
+brew install --cask kitty || {
+	error "Erro ao instalar o terminal Kitty."
 	exit 1
 }
 
 ###############################################################################
 
 # Instalando asdf
-info "Instalando asdf..."
-brew install asdf || {
-	error "Erro ao instalar asdf."
-	exit 1
-}
+
+install "asdf"
 
 # Configurando o asdf no shell
 if ! grep -q "asdf.sh" ~/.zshrc; then
 	info "Adicionando asdf ao Zsh..."
-	echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >>$profile
-	echo -e "\n. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash" >>$profile
-
+	echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> $profile
+	echo -e "\n. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash" >> $profile
+	
 	source $profile
 fi
 
 ###############################################################################
 
 # Instalando nvm
-info "Instalando nvm..."
-brew install nvm || {
-	error "Erro ao instalar nvm."
-	exit 1
-}
+
+install "nvm"
 
 # Configurando o nvm no shell
 if ! grep -q ".nvm" $profile; then
@@ -78,12 +113,5 @@ if ! grep -q ".nvm" $profile; then
 fi
 
 ###############################################################################
-
-# Instalando o terminal Kitty
-info "Instalando o terminal Kitty..."
-brew install --cask kitty || {
-	error "Erro ao instalar o terminal Kitty."
-	exit 1
-}
 
 info "Instalação concluída! 🎉"
