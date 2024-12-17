0c575bb5c1e4e8c7d98c7d9c5a73cec04ad31954
Zulian <luiz.zulian@kovi.com.br>
2024-12-17 10:23:44 -0300

    update lazyvim and migrate nvim-cmp to blink.cmp

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -35,61 +35,154 @@ end
 
 return {
 	{
-		"hrsh7th/nvim-cmp",
+		"saghen/blink.cmp",
+		version = not vim.g.lazyvim_blink_main and "*",
+		build = vim.g.lazyvim_blink_main and "cargo build --release",
+		opts_extend = {
+			"sources.completion.enabled_providers",
+			"sources.compat",
+			"sources.default",
+		},
 		dependencies = {
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-path" },
-			{ "hrsh7th/cmp-buffer" },
+			"rafamadriz/friendly-snippets",
+			-- add blink.compat to dependencies
 			{
-				"L3MON4D3/LuaSnip",
-				dependencies = {
-					"saadparwaiz1/cmp_luasnip",
-					"rafamadriz/friendly-snippets",
-				},
+				"saghen/blink.compat",
+				optional = true, -- make optional so it's only enabled if any extras need it
+				opts = {},
+				version = not vim.g.lazyvim_blink_main and "*",
 			},
 		},
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+		event = "InsertEnter",
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			snippets = {
+				expand = function(snippet, _)
+					return LazyVim.cmp.expand(snippet)
+				end,
+			},
+			appearance = {
+				-- sets the fallback highlight groups to nvim-cmp's highlight groups
+				-- useful for when your theme doesn't support blink.cmp
+				-- will be removed in a future release, assuming themes add support
+				use_nvim_cmp_as_default = false,
+				-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
+			},
+			completion = {
+				accept = {
+					-- experimental auto-brackets support
+					auto_brackets = {
+						enabled = true,
+					},
 				},
-
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
+				menu = {
+					draw = {
+						treesitter = { "lsp" },
+					},
 				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 200,
+				},
+				ghost_text = {
+					enabled = vim.g.ai_cmp,
+				},
+			},
 
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
+			-- experimental signature help support
+			-- signature = { enabled = true },
 
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					-- { name = "luasnip" }, -- For luasnip users.
-					{ name = "buffer" },
-					{ name = "path" },
-				}),
+			sources = {
+				-- adding any nvim-cmp sources here will enable them
+				-- with blink.compat
+				compat = {},
+				default = { "lsp", "path", "snippets", "buffer" },
+				cmdline = {},
+			},
 
-				formatting = {
-					expandable_indicator = true,
-					fields = { "abbr", "kind" },
-					format = function(_, vim_item)
-						if icons[vim_item.kind] then
-							vim_item.kind = icons[vim_item.kind] .. " " .. vim_item.kind or vim_item.kind
+			keymap = {
+				preset = "enter",
+				["<C-y>"] = { "select_and_accept" },
+			},
+		},
+		---@param opts blink.cmp.Config | { sources: { compat: string[] } }
+		config = function(_, opts)
+			-- setup compat sources
+			local enabled = opts.sources.default
+			for _, source in ipairs(opts.sources.compat or {}) do
+				opts.sources.providers[source] = vim.tbl_deep_extend(
+					"force",
+					{ name = source, module = "blink.compat.source" },
+					opts.sources.providers[source] or {}
+				)
+				if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
+					table.insert(enabled, source)
+				end
+			end
+
+			-- add ai_accept to <Tab> key
+			if not opts.keymap["<Tab>"] then
+				if opts.keymap.preset == "super-tab" then -- super-tab
+					opts.keymap["<Tab>"] = {
+						require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
+						LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+						"fallback",
+					}
+				else -- other presets
+					opts.keymap["<Tab>"] = {
+						LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+						"fallback",
+					}
+				end
+			end
+
+			---  NOTE: compat with latest version. Currenlty 0.7.6
+			if not vim.g.lazyvim_blink_main then
+				---@diagnostic disable-next-line: inject-field
+				opts.sources.completion = opts.sources.completion or {}
+				opts.sources.completion.enabled_providers = enabled
+				if vim.tbl_get(opts, "completion", "menu", "draw", "treesitter") then
+					---@diagnostic disable-next-line: assign-type-mismatch
+					opts.completion.menu.draw.treesitter = true
+				end
+			end
+
+			-- Unset custom prop to pass blink.cmp validation
+			opts.sources.compat = nil
+
+			-- check if we need to override symbol kinds
+			for _, provider in pairs(opts.sources.providers or {}) do
+				---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
+				if provider.kind then
+					local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+					local kind_idx = #CompletionItemKind + 1
+
+					CompletionItemKind[kind_idx] = provider.kind
+					---@diagnostic disable-next-line: no-unknown
+					CompletionItemKind[provider.kind] = kind_idx
+
+					---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
+					local transform_items = provider.transform_items
+					---@param ctx blink.cmp.Context
+					---@param items blink.cmp.CompletionItem[]
+					provider.transform_items = function(ctx, items)
+						items = transform_items and transform_items(ctx, items) or items
+						for _, item in ipairs(items) do
+							item.kind = kind_idx or item.kind
 						end
-						return vim_item
-					end,
-				},
-			})
+						return items
+					end
+
+					-- Unset custom prop to pass blink.cmp validation
+					provider.kind = nil
+				end
+			end
+
+			require("blink.cmp").setup(opts)
 		end,
 	},
 }
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,157 +1,160 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			local mason = require("mason")
-			mason.setup({
-				ensure_installed = require("language").mason.setup(),
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		config = function()
-			require("config.utils").notify.info("MASON TOOLS", "Setup")
-			require("mason-tool-installer").setup({
-				ensure_installed = require("language").mason.setup(),
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      local mason = require("mason")
+      mason.setup({
+        ensure_installed = require("language").mason.setup(),
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    config = function()
+      require("config.utils").notify.info("MASON TOOLS", "Setup")
+      require("mason-tool-installer").setup({
+        ensure_installed = require("language").mason.setup(),
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "saghen/blink.cmp" },
+    lazy = false,
+    config = function()
+      -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local function on_attach(client, bufnr)
-				local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-				--if client.supports_method("textDocument/formatting") then
-				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					group = augroup,
-					buffer = bufnr,
-					callback = function()
-						-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-						-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+      local function on_attach(client, bufnr)
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        --if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+            -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
 
-						--vim.lsp.buf.formatting_sync()
-						vim.lsp.buf.format({
-							bufnr = bufnr,
-							async = false,
-						})
-					end,
-				})
-				-- end -- if client.supports_method("textDocument/formatting")
+            --vim.lsp.buf.formatting_sync()
+            vim.lsp.buf.format({
+              bufnr = bufnr,
+              async = false,
+            })
+          end,
+        })
+        -- end -- if client.supports_method("textDocument/formatting")
 
-				-- Enable completion triggered by <c-x><c-o>
-				-- vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+        -- Enable completion triggered by <c-x><c-o>
+        -- vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-				-- Mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
-				local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        -- Mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-				vim.keymap.set("n", "gK", vim.lsp.buf.hover, bufopts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+        vim.keymap.set("n", "gK", vim.lsp.buf.hover, bufopts)
 
-				-- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-				-- vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-				-- vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-				-- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-				-- vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
-				-- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-				-- vim.keymap.set("n", "gS", vim.lsp.buf.signature_help, bufopts)
-				-- vim.keymap.set("n", "rn", vim.lsp.buf.rename, bufopts)
-				-- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-				-- vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-				-- vim.keymap.set("n", "<space>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, bufopts)
-				-- vim.keymap.set("n", "gtd", vim.lsp.buf.type_definition, bufopts)
-				-- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-				vim.keymap.set("n", "<RightMouse>", vim.lsp.buf.code_action, bufopts)
-				-- vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, bufopts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-				vim.keymap.set("n", "<leader>lr", ":Telescope lsp_references", bufopts)
-				vim.keymap.set("n", "<leader>fm", function()
-					vim.lsp.buf.format({ async = true })
-				end, bufopts)
-			end
+        -- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        -- vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+        -- vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+        -- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        -- vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
+        -- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+        -- vim.keymap.set("n", "gS", vim.lsp.buf.signature_help, bufopts)
+        -- vim.keymap.set("n", "rn", vim.lsp.buf.rename, bufopts)
+        -- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+        -- vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+        -- vim.keymap.set("n", "<space>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, bufopts)
+        -- vim.keymap.set("n", "gtd", vim.lsp.buf.type_definition, bufopts)
+        -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set("n", "<RightMouse>", vim.lsp.buf.code_action, bufopts)
+        -- vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, bufopts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+        vim.keymap.set("n", "<leader>lr", ":Telescope lsp_references", bufopts)
+        vim.keymap.set("n", "<leader>fm", function()
+          vim.lsp.buf.format({ async = true })
+        end, bufopts)
+      end
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			require("language").lsp.setup(lspconfig, capabilities, on_attach)
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			-- Editor settings for LSP custom signs
-			local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-			end
+      require("language").lsp.setup(lspconfig, capabilities, on_attach)
 
-			-- Dialog box for LSP visual setup
-			-- vim.diagnostic.config({
-			-- 	virtual_text = true,
-			-- 	signs = true,
-			-- 	underline = true,
-			-- 	update_in_insert = false,
-			-- 	severity_sort = false,
-			-- })
+      -- Editor settings for LSP custom signs
+      local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
 
-			vim.diagnostic.config({
-				-- Show diagnostic message using virtual text.
-				virtual_text = false,
-				-- Show a sign next to the line with a diagnostic.
-				signs = true,
-				-- Update diagnostics while editing in insert mode.
-				update_in_insert = true,
-				-- Use an underline to show a diagnostic location.
-				underline = true,
-				-- Order diagnostics by severity.
-				severity_sort = false,
-				-- Show diagnostic messages in floating windows.
-				float = {
-					border = "rounded",
-					source = true,
-					-- source = "always",
-					-- header = '',
-					-- prefix = '',
-					-- Credit: https://github.com/jessarcher/dotfiles/blob/master/nvim/lua/user/plugins/lspconfig.lua
-					format = function(diagnostic)
-						if diagnostic.user_data ~= nil and diagnostic.user_data.lsp.code ~= nil then
-							return string.format("%s: %s", diagnostic.user_data.lsp.code, diagnostic.message)
-						end
-						return diagnostic.message
-					end,
-				},
-			})
+      -- Dialog box for LSP visual setup
+      -- vim.diagnostic.config({
+      -- 	virtual_text = true,
+      -- 	signs = true,
+      -- 	underline = true,
+      -- 	update_in_insert = false,
+      -- 	severity_sort = false,
+      -- })
 
-			-- to show full diagnostic message on hover line diagnostic
-			-- You will likely want to reduce updatetime which affects CursorHold
-			-- note: this setting is global and should be set only once
-			vim.o.updatetime = 250
-			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-				group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
-				callback = function()
-					if vim.fn.mode() == "n" then
-						vim.diagnostic.open_float(nil, { focus = false })
-					end
-				end,
-			})
-		end,
-	},
+      vim.diagnostic.config({
+        -- Show diagnostic message using virtual text.
+        virtual_text = false,
+        -- Show a sign next to the line with a diagnostic.
+        signs = true,
+        -- Update diagnostics while editing in insert mode.
+        update_in_insert = true,
+        -- Use an underline to show a diagnostic location.
+        underline = true,
+        -- Order diagnostics by severity.
+        severity_sort = false,
+        -- Show diagnostic messages in floating windows.
+        float = {
+          border = "rounded",
+          source = true,
+          -- source = "always",
+          -- header = '',
+          -- prefix = '',
+          -- Credit: https://github.com/jessarcher/dotfiles/blob/master/nvim/lua/user/plugins/lspconfig.lua
+          format = function(diagnostic)
+            if diagnostic.user_data ~= nil and diagnostic.user_data.lsp.code ~= nil then
+              return string.format("%s: %s", diagnostic.user_data.lsp.code, diagnostic.message)
+            end
+            return diagnostic.message
+          end,
+        },
+      })
+
+      -- to show full diagnostic message on hover line diagnostic
+      -- You will likely want to reduce updatetime which affects CursorHold
+      -- note: this setting is global and should be set only once
+      vim.o.updatetime = 250
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+        callback = function()
+          if vim.fn.mode() == "n" then
+            vim.diagnostic.open_float(nil, { focus = false })
+          end
+        end,
+      })
+    end,
+  },
 }
